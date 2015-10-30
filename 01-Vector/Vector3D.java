2b4d3fd4050b8482190e2b8eb66b526df05a955e
Alfithra Reynaldi <aldivegito25@gmail.com>
2015-10-30 17:17:14 +0700

    Praktikum 11

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -1,9 +1,9 @@
 
 public class Vector3D {
     
-    public double x;
-    public double y;
-    public double z;
+    private double x;
+    private double y;
+    private double z;
     
     public Vector3D (double x, double y, double z){
         this.x=x;
@@ -11,6 +11,16 @@ public class Vector3D {
         this.z=z;
         
     }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+    public double getZ() {
+        return z;
+    }
 }
-
-//By: PomPom
