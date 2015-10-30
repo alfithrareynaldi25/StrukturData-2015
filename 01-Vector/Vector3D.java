1507f189ec24ea1d4035baa6f6dee92a44da7827
Alfithra Reynaldi <aldivegito25@gmail.com>
2015-10-30 16:26:00 +0700

    no message

---
+++ 01-Vector/Vector3D.java
@@ -0,0 +1,15 @@
+
+public class Vector3D {
+    
+    public double x;
+    public double y;
+    public double z;
+    
+    public Vector3D (double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+        
+    }
+}
+    
