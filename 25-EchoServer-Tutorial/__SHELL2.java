f15144e074411462df6641328f9f40a02c566edd
Alfithra Reynaldi <aldivegito25@gmail.com>
2015-12-11 15:51:49 +0700

    Tutorial

--- 25-EchoServer-Tutorial/__SHELL2.java
+++
@@ -1,8 +0,0 @@
-
-public class __SHELL2 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = { };
-EchoServer.main(__bluej_param0);
-
-}}
